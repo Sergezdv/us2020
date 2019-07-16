03d681724a28c7e8eebd8c5b45f459a8e4155299
Sergezdv <snz985@gmail.com>
2019-07-16 23:31:03 +0300

    set color for each candidates from first chart https://trello.com/c/4IuH4PZo

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,19 @@ $(document).ready(function () {
         'packages': ['corechart']
     });
 
+    let firstChart;
+    const cand_colors = {};
+    const get_cand_colors = () => {
+        firstChart.ba.ia.F.forEach(p => {
+            cand_colors[p.title] = {color: p.color.color, darker: p.color.wb}
+        });
+    };
 
     const titleTextStyle = { fontName: '', fontSize: 16, color: '#404040', italic: true, bold: false };
 
     // chart_div1
     // chart_div2
-    const drawPieChart = (id, title) => (() => {
+    const drawPieChart = (id, title, cb) => (() => {
         var chart_data0 = eval(`chart_${id}_data`);
         var chart_data = $.map(chart_data0, (v, k) => ([[k, v]]));
         var values = [['Name', 'Result']].concat(chart_data);
@@ -28,14 +35,21 @@ $(document).ready(function () {
         };
 
         var chart = new google.visualization.PieChart(document.getElementById(`chart_div${id}`));
+
+        if (id === 1) {
+            firstChart = chart;
+            google.visualization.events.addListener(chart, 'ready', () => { get_cand_colors(); cb(); })
+        } else {
+            options.colors = Object.keys(chart_data0).map(c => cand_colors[c])
+        }
+
         chart.draw(data, options);
     });
 
-
     // chart_div3
     const drawChart3 = () => {
-        var chart_data = $.map(chart_3_data, (v, k) => ([[k, v, Math.floor(v) + '%']]));
-        var values = [['Name', 'Result ' + '%', {role: 'annotation'}]].concat(chart_data);
+        var chart_data = $.map(chart_3_data, (v, k) => ([[k, v, Math.floor(v) + '%', cand_colors[k].color]]));
+        var values = [['Name', 'Result ' + '%', {role: 'annotation'}, {role: 'style'}]].concat(chart_data);
         var data = google.visualization.arrayToDataTable(values);
         var options = {
             title: 'Likelihood of Winning vs. Republican Candidate',
@@ -79,6 +93,7 @@ $(document).ready(function () {
             title,
             titleTextStyle,
             chartArea: {left: 25, width: '55%'},
+            colors: candidates.map(c => cand_colors[c].color)
         };
 
         var chart = new google.visualization.LineChart(document.getElementById(`chart_div${id}history`));
@@ -86,12 +101,15 @@ $(document).ready(function () {
     });
 
     function buildCharts() {
-        google.setOnLoadCallback(drawPieChart(1, 'Chances of Winning 2020 Election, based on odds from different betting websites'));
-        google.setOnLoadCallback(drawPieChart(2, 'Chances of Being 2020 Democratic Candidate, based on odds from different betting websites'));
-        google.setOnLoadCallback(drawChart3);
-        google.setOnLoadCallback(drawChartHist(1, 'Chances of Winning 2020 Election over time'));
-        google.setOnLoadCallback(drawChartHist(2, 'Democratic nominee likelihood over time'));
-        google.setOnLoadCallback(drawChartHist(3, 'Winning vs. republican candidate nominee likelihood over time'));
+        google.setOnLoadCallback(drawPieChart(1, 'Chances of Winning 2020 Election, based on odds from different betting websites',
+            function() {
+                google.setOnLoadCallback(drawPieChart(2, 'Chances of Being 2020 Democratic Candidate, based on odds from different betting websites'));
+                google.setOnLoadCallback(drawChart3);
+                google.setOnLoadCallback(drawChartHist(1, 'Chances of Winning 2020 Election over time'));
+                google.setOnLoadCallback(drawChartHist(2, 'Democratic nominee likelihood over time'));
+                google.setOnLoadCallback(drawChartHist(3, 'Winning vs. republican candidate nominee likelihood over time'));
+            }
+        ));
     }
 
     buildCharts();
