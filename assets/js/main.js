7a0728cf967c327bbdd8c949511a7fa52611a5a6
Sergezdv <snz985@gmail.com>
2019-08-28 22:12:09 +0300

    add GA

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ function toggleText() {
         dots.style.display = "none";
         btnText.innerHTML = "Less";
         moreText.style.display = "inline";
+        gtag('event', 'click', { 'show_more': 'top_probabilities_text' });
     }
 }
 
